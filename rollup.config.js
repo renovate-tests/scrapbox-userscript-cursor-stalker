474f247e543c030f5612be6297d374f757e513ce
mizdra <pp.mizdra@gmail.com>
2021-06-20 17:36:21 +0900

    eslint の警告を修正

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,8 @@ import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 
-// eslint-disable-next-line import/no-default-export
 /** @type {import('rollup').RollupOptions} */
-export default {
+const config = {
   input: 'src/index.ts',
   output: {
     dir: 'dist',
@@ -18,3 +17,6 @@ export default {
     typescript({ tsconfig: 'tsconfig.src.json', sourceMap: false }),
   ],
 };
+
+// eslint-disable-next-line import/no-default-export
+export default config;
